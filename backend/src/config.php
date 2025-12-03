3ec82dc6f6600635009c35b958df7ce4e3ed32f2
Stanislav Křikava <krikava@medirecord.cz>
2025-12-03 12:55:50 +0100

    feat: backend from php to express.js

--- backend/src/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$C = [];
-$C['DB_HOST'] = getenv('DB_HOST') ?: '127.0.0.1';  
-$C['DB_PORT'] = getenv('DB_PORT') ?: '5432';
-$C['DB_NAME'] = 'community';
-$C['DB_USER'] = 'stanislav';
-$C['DB_PASS'] = 'heslo';
-$C['JWT_SECRET'] = '1234';
